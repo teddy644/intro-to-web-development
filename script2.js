1ea3946f7dc4d64cf31a8fce8b20a535c93ce3c8
Mariza teddy <marizateddy4@gmail.com>
2025-05-17 12:08:19 +0200

    i deleted message

--- script2.js
+++ script2.js
@@ -1,1 +1,16 @@
-console,log(5);
+
+// Declare variables to store input values
+var num1, num2;
+
+// Uncomment the following lines to take input from the user
+// num1 = window.prompt("Input the First integer", "0");
+// num2 = window.prompt("Input the second integer", "0");
+
+// Compare the values and print the larger or equality 
+if (parseInt(num1, 10) > parseInt(num2, 10)) {
+  console.log("The larger of " + num1 + " and " + num2 + " is " + num1 + ".");
+} else if (parseInt(num2, 10) > parseInt(num1, 10)) {
+  console.log("The larger of " + num1 + " and " + num2 + " is " + num2 + ".");
+} else {
+  console.log("The values " + num1 + " and " + num2 + " are equal.");
+}
